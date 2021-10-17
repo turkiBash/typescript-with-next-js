61b7d70777f1f012bd270429d509bca559793795
تركي بن بشير <dev@turki.tech>
2021-10-17 20:42:57 +0300

    initial

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { Flex,Box, Heading } from "@chakra-ui/layout";
+import Link from "next/link"
+
+
+const Navbar = () => {
+
+    return (
+
+        <Flex direction="row" bgColor="teal.700">
+            <Heading>Hi</Heading>
+        </Flex>
+    )
+}
+
+export default Navbar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '../components/Navbar'
+
+
+ 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Navbar/>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 export default MyApp
+
+
+
