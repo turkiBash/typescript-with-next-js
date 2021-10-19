fc3e926d28abe738835b6e1f75f29fb64f72086f
turkibash <dev@turki.tech>
2021-10-19 13:36:43 +0300

    sidebar ENH

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,19 +8,44 @@ const Sidebar = () => {
 
     return (
 
-        <Flex as="nav" flexDirection="column" bgColor="teal.700" height="100vh" width="15%" >
-            <Flex flexDirection="column" alignItems="center" >
-                <Avatar size="lg" mt="10px"></Avatar>
-                <Text as="h3" size="md" mt={2} color="white">Turki Albasheer</Text>
+        <Flex
+            as="nav"
+            flexDirection="column"
+            bgColor="teal.700"
+            height="100vh"
+            width="15%"
+            pos="sticky"
+            boxShadow="xl"
+
+        >
+            <Flex
+                flexDirection="column"
+                alignItems="center"
+
+            >
+                <Avatar
+                    size="lg"
+                    mt="10px"
+                ></Avatar>
+                <Text
+                    as="h3"
+                    size="md"
+                    mt='10px'
+                    color="white"
+                >Turki Albasheer</Text>
             </Flex>
-            <Divider />
-            <Flex as="nav" height="70%" flexDirection="column" alignItems="center"mt={4} justifyContent="space-evenly">
-                {/* <Flex flexDirection="column" bgColor="blue.200"> */}
-                <Button as="a">Home</Button>
-                <Button as="a">Posts</Button>
-                <Button as="a">Profile</Button>
-                <Button as="a">Contact</Button>
-                {/* </Flex> */}
+            <Divider mt='10px' />
+            <Flex
+                as="nav"
+                height="200px"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="flex-start"
+            >
+                <Button variant="ghost" width="100%" as="a">Home</Button>
+                <Button variant="ghost" width="100%" as="a">Posts</Button>
+                <Button variant="ghost" width="100%" as="a">Profile</Button>
+                <Button variant="ghost" width="100%" as="a">Contact</Button>
             </Flex>
         </Flex>
     )
